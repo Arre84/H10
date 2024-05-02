e918afaebaf095f4ede6d06dafe0a649108d3939
Arre84 <emilio.arredondop@udem.edu>
2024-05-01 19:50:37 -0600

    2nd commit
    Added the bounding box and centroid of the image

--- object_tracking.py
+++ object_tracking.py
@@ -3,6 +3,8 @@ import cv2 as cv
 import argparse
 from numpy.typing import NDArray
 
+previous = []
+
 def user_interaction() ->argparse.ArgumentParser:
     """
     Interact with the user to get the path of the image containing the object to track, video to do the tracking
@@ -88,10 +90,10 @@ def extract_corners_and_descriptors(image:NDArray)-> tuple[list[cv.KeyPoint], ND
     """
 
     # Harris corner detection/
-    harris_corners1 = cv.cornerHarris(image, blockSize=2, ksize=3, k=0.04)
+    harris_corners1 = cv.cornerHarris(image, blockSize=15, ksize=11, k=0.04)
 
 # Threshold for corner detection
-    threshold = 0.15 * harris_corners1.max()
+    threshold = 0.1 * harris_corners1.max()
     thresholded_img1 = np.zeros_like(image)
     thresholded_img1[harris_corners1 > threshold] = 255
 #Extract keypoints
@@ -107,13 +109,12 @@ def extract_corners_and_descriptors(image:NDArray)-> tuple[list[cv.KeyPoint], ND
     kp, des = sift.compute(image,keypoints1)
     return kp, des
 
-def draw_matches(img1: np.ndarray, img2: np.ndarray, descriptors1: np.ndarray, descriptors2: np.ndarray, kp1: list[cv.KeyPoint], kp2: list[cv.KeyPoint]):
+def draw_matches(img2: np.ndarray, descriptors1: np.ndarray, descriptors2: np.ndarray, kp1: list[cv.KeyPoint], kp2: list[cv.KeyPoint]):
     """
     Draws matches between two images based on their descriptors and keypoints using RANSAC-based feature matching.
 
     Parameters:
-        img1: Original Image
-        img2: Image to compare or to match the first image
+        img2: Tracking frame
         descriptors1: List of descriptors of the first image
         descriptors1: List of descriptors of the second image
         kp1: List containing the keypoints of the first image
@@ -131,28 +132,75 @@ def draw_matches(img1: np.ndarray, img2: np.ndarray, descriptors1: np.ndarray, d
     # Apply RANSAC to filter out outliers
     src_pts = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
     dst_pts = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
-    M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
+    M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 10)
 
     # Filter matches using RANSAC mask
     matches = [matches[i] for i in range(len(matches)) if mask[i] == 1]
-
+    x_coordinates = []
+    y_coordinates= []
     # Draw match points only on the second image
     img_matches = img2.copy()  # Create a copy of the second image
     for match in matches:
         pt2 = kp2[match.trainIdx].pt
+        x_coordinates.append(pt2[0])  # Agregar la coordenada x a la lista
+        y_coordinates.append(pt2[1])  # Agregar la coordenada y a la lista
         cv.circle(img_matches, (int(pt2[0]), int(pt2[1])),2, (0, 255, 0), -1)  # Draw a circle at the matched keypoint
-    
+
+    # Drawing middle line
     # Get image dimensions
     height, width, _ = img2.shape
-
     # Define the starting point at the top of the image (horizontally centered)
     pt1 = (int(width / 2), 0)
-
     # Define the ending point at the bottom of the image (horizontally centered)
     pt2 = (int(width / 2), height - 1)
     cv.line(img_matches,pt1,pt2,(255,0,0),1)
+
+    #Draw bounding box, centroid and obtain max coordinates
+    if x_coordinates and y_coordinates:
+        rectangle(x_coordinates, y_coordinates, img_matches)
+    #Show current frame
     cv.imshow('Matched Features', img_matches)
     return match
+
+def rectangle(x: np.ndarray, y: np.ndarray, image: np.ndarray):
+    """
+    Draws the bounding box and computes and draws the centroid of the object.
+
+    Parameters:
+        x: x coordinates of the detected keypoints
+        y: y coordinates of the detected keypoints
+        image: frame to compute the drawings
+
+    Returns:
+        center of the object
+    """
+    global previous
+    xmin = int(min(x))
+    xmax = int(max(x))
+    ymin = int(min(y))
+    ymax = int(max(y))
+    pt1 = (xmin, ymin)  # Bottom-left corner
+    pt2 = (xmax, ymax)  # Top-right corner
+    cv.rectangle(image, pt1, pt2, (255, 0, 0), 1)
+    # Calculate the center for drawing the circle
+    center_x = (pt1[0] + pt2[0]) // 2
+    center_y = (pt1[1] + pt2[1]) // 2
+    centroid = (center_x,center_y)
+    current = estimate_centroid(centroid,previous)
+    previous = current
+    cv.circle(image, (current[0], current[1]), 2, (0, 0, 255), 2)
+    return None
+
+def estimate_centroid(current: tuple[int, int], previous: tuple[int, int], smoothing_factor: float = 0.2) -> tuple[int, int]:
+    if current and previous:
+        # Interpolate between current and previous centroid positions based on the smoothing factor
+        smoothed_x = int(previous[0] + (current[0] - previous[0]) * smoothing_factor)
+        smoothed_y = int(previous[1] + (current[1] - previous[1]) * smoothing_factor)
+
+        return smoothed_x, smoothed_y
+    else:
+        return current
+
 
 def pipeline()->None:
     args = user_interaction()
@@ -170,7 +218,7 @@ def pipeline()->None:
         rescale_frame = resize(frame,args.resize)
         gray_frame = grayscale(rescale_frame)
         kp, des = extract_corners_and_descriptors(gray_frame)
-        draw_matches(img_obj, rescale_frame, des_obj, des, kp_obj,kp)
+        draw_matches(rescale_frame, des_obj, des, kp_obj,kp)
         key = cv.waitKey(20)
         if key == ord('q') or key == 27:
             print("Programm finished!")
